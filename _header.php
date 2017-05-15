f9c6292216acc686dfe67804f9d8e54f864f4aab
Martynas <martynasjucys@gmail.com>
2017-05-15 20:55:48 +0300

    Header blocks and small fixes

--- _header.php
+++ _header.php
@@ -11,20 +11,53 @@
     <link rel="stylesheet" href="dist/style.css">
 </head>
 <body>
-
     <header class="header">
         <div class="container">
-            <div class="site-logo float-left">
-                <a href="#"><img src="images/logo.png" alt="B.A.D.S"></a>
+            <div class="float-left">
+                <a href="#" class="site-logo"><img src="images/logo.png" alt="B.A.D.S"></a>
             </div>
-            <div class="top-nav-wrap float-right">
-                <ul class="top-nav">
-                    <li><a href="#">Pristatymas</a></li>
-                    <li><a href="#">Apmokėjimas</a></li>
-                    <li><a href="#">Garantijos ir grąžinimas</a></li>
-                    <li><a href="#">Apie B.A.D.S</a></li>
-                    <li><a href="#">Kontaktai</a></li>
-                </ul>
+            <div class="float-right">
+                <div class="top-nav-wrap float-right">
+                    <ul class="top-nav">
+                        <li><a href="#">Pristatymas</a></li>
+                        <li><a href="#">Apmokėjimas</a></li>
+                        <li><a href="#">Garantijos ir grąžinimas</a></li>
+                        <li><a href="#">Apie B.A.D.S</a></li>
+                        <li><a href="#">Kontaktai</a></li>
+                    </ul>
+                </div>
+                <div class="clearfix"></div>
+                <div class="top-info-blocks float-right">
+                    <div class="info-block float-left">
+                        <div class="block-image">
+                            <img src="images/phone.png" alt="Telephone"/>
+                        </div>
+                            
+                        <div class="info-block-content">
+                            <span><strong>Kilus klausimams skambinkite: <a href="tel:86 5759X55">86 5759X55</a></strong></span>
+                            <div></div>
+                            <span>Darbo dienomis 8:00-20:00</span>
+                        </div>
+                    </div>
+
+                    <div class="info-block float-left">
+                        <div class="info-block-content">
+                            <span>Pasirinkite valiutą:</span>
+                            
+                            <a href="#" class="current">LT</a>
+                            <a href="#">LV</a>
+                            <a href="#">EUR</a>
+                        </div>
+                        <div class="spacer"></div>
+                        <div class="block-image">
+                            <img src="images/account.png" alt="Account"/>
+                        </div>
+                        <div class="info-block-content">
+                            <a href="mailto:vardenis@gmail.com">vardenis@gmail.com</a>
+                            <a href="#">X</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </header>     
